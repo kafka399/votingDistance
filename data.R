d470affbaba74ff04e770bdb8b14c7769c688558
kafka399 <kafka399@gmail.com>
2012-10-22 22:47:27 +0200

    csv & pyc remove

--- data.R
+++ data.R
@@ -1,38 +1,25 @@
 setwd('/home/git/seimunai/')
 require(reshape)
 require(ggplot2)
-vote=read.table('getdata/items.csv',encoding='utf-8',sep=',',header=T,stringsAsFactors=F)
+vote=read.table('getdata/items14000.csv',encoding='utf-8',sep=',',header=T,stringsAsFactors=F)
 #vote=vote[which(vote$url_id<5035),]
-tmp=as.character(vote[which(vote$url_id>9530),]$partija)
+#vote=vote[which(vote$url_id>=9520),]
+parl_names=as.factor(as.character(vote$pavarde))
+parl_party=vote
 #vote=data.matrix(dcast(vote,seimunas+partija~url_id,value.var=('balsas') ))
-factor=as.factor(vote$partija)
-if(length(grep('AŽF',levels(factor)))>0)
-  levels(factor)[grep('AŽF',levels(factor))]=levels(factor)[grep('LCSF',levels(factor))]
 
-if(length(grep('TPPF',levels(factor)))>0)
-  levels(factor)[grep('TPPF',levels(factor))]=levels(factor)[grep('LCSF',levels(factor))]
-
-
-if(length(grep('JF',levels(factor)))>0)
-  levels(factor)[grep('JF',levels(factor))]=levels(factor)[grep('LCSF',levels(factor))]
-
-if(length(grep('TSF',levels(factor)))>0)
-  levels(factor)[grep('TSF',levels(factor))]=levels(factor)[grep('TSLKDF',levels(factor))]
-
-if(length(grep('NSF',levels(factor)))>0)
-  levels(factor)[grep('TSF',levels(factor))]=levels(factor)[grep('LSDPF',levels(factor))]
-
-if(length(grep('VLF',levels(factor)))>0)
-  levels(factor)[grep('VLF',levels(factor))]=levels(factor)[grep('KPF',levels(factor))]
-
-levels(factor)[which(sapply(levels(factor),nchar)==2)]=c("NO PARTY")
-vote$partija=factor
-vote=data.matrix(dcast(vote,partija~url_id,value.var=('balsas') ))
-rownames(vote)=levels(factor)
+vote$pavarde=parl_names
+tmp=vote
+vote=data.matrix(dcast(vote,pavarde~url_id,value.var=('balsas') ))
+rownames(vote)=levels(parl_names)
+vote=vote[-which(apply(vote,1, function(x) any(is.na(x)))),]
 vote=dist(vote %*% t(vote))
+
 vote=cmdscale(vote)
-vote=data.frame(x=as.numeric(vote[,1]),y=as.numeric(vote[,2]),party=rownames(vote))
+vote=data.frame(x=as.numeric(vote[,1]),y=as.numeric(vote[,2]),member=rownames(vote))
+vote=cbind(vote,party=sapply(vote$member,function(x) parl_party$partija[last(which(as.character(x)==parl_party$pavarde))]))
+vote$member = paste(vote$member,vote$party,sep='-')
 #plot(vote)
 #text(vote,tmp)
 
-ggplot(vote,aes(x,y,color=party))+geom_text(aes(label=party))
+ggplot(vote,aes(x,y,color=party))+geom_text(aes(label=member))
