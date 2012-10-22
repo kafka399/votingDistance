70e02e75b23ba3af8cd51031711d7a987a348320
kafka399 <kafka399@gmail.com>
2012-10-22 22:50:27 +0200

    clean up

--- data.R
+++ data.R
@@ -2,11 +2,10 @@ setwd('/home/git/seimunai/')
 require(reshape)
 require(ggplot2)
 vote=read.table('getdata/items14000.csv',encoding='utf-8',sep=',',header=T,stringsAsFactors=F)
-#vote=vote[which(vote$url_id<5035),]
-#vote=vote[which(vote$url_id>=9520),]
+vote=rbind(vote,read.table('getdata/items12000.csv',encoding='utf-8',sep=',',header=T,stringsAsFactors=F))
+
 parl_names=as.factor(as.character(vote$pavarde))
 parl_party=vote
-#vote=data.matrix(dcast(vote,seimunas+partija~url_id,value.var=('balsas') ))
 
 vote$pavarde=parl_names
 tmp=vote
@@ -19,7 +18,6 @@ vote=cmdscale(vote)
 vote=data.frame(x=as.numeric(vote[,1]),y=as.numeric(vote[,2]),member=rownames(vote))
 vote=cbind(vote,party=sapply(vote$member,function(x) parl_party$partija[last(which(as.character(x)==parl_party$pavarde))]))
 vote$member = paste(vote$member,vote$party,sep='-')
-#plot(vote)
-#text(vote,tmp)
+
 
 ggplot(vote,aes(x,y,color=party))+geom_text(aes(label=member))
